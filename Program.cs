f648c8f132060147b59036a64852617414df67d5
RomaSamkov <Romansamkov.rs@gmail.com>
2025-11-10 20:29:29 +0200

    add variables and types

--- Program.cs
+++ Program.cs
@@ -7,8 +7,15 @@ namespace CSharpPlayground
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            Console.Read();
+            int number = 12;
+            uint num1 = 10;
+            byte num2 = 20;
+            long bigNumber = 123456;
+
+            Console.WriteLine(num1 + num2 + bigNumber);
+            Console.WriteLine(number);
+            number = 41;
+            Console.WriteLine("Result: " + number);
         }
     }
 }
